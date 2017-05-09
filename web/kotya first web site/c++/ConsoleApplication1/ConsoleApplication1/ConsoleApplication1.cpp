8ab8c2cda40fa320955a6c29ce15a9a65e701208
SelikaAndKotya <SelikaVictory@yandex.ru>
2017-05-09 13:57:45 +0300

    Смартгит опять не хочет принимать папку(
     гружу через сайт

---
+++ web/kotya first web site/c++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	cout << "A =" ;
+	cin >> a;
+	cout << "B =" ;
+	cin >> b;
+	c =sqrt (a *a + b *b);
+
+		cout << "C =" ;
+	cout << c  ;
+
+}
