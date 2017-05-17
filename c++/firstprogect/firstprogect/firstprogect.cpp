65fe4137bfb5bce63f86c81b5d2a055b6d7553ab
kotya <3962756@gmail.com>
2017-05-17 12:45:20 +0300

    перенесено из веба

---
+++ c++/firstprogect/firstprogect/firstprogect.cpp
@@ -0,0 +1,26 @@
+﻿// firstprogect.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	
+		double a, b, c;
+		cout << "A =";
+		cin >> a;
+		cout << "B =";
+		cin >> b;
+		c = sqrt(a *a + b *b);
+		cout << "C =";
+		cout << c << endl;
+		cout << "done";
+		
+		
+		return 0;
+					
+	}
+
