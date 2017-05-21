8994dc913fe75ef44b21f95a17c1a052392f4b69
kotya <3962756@gmail.com>
2017-05-21 14:21:18 +0300

    косячный хелловорлд

---
+++ c++/chapter1/chapter1/chapter1.cpp
@@ -0,0 +1,16 @@
+// chapter1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+
+
+int main()
+{
+   
+	cout << "Hello world";
+
+		return 0;
+}
+
